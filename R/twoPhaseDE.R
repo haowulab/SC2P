2643181474f4a7db8bcf822625bb1a623a340629
hwu30 <hwu30@18MB-BIOS009.local>
2018-01-22 10:54:47 -0500

    update function helps

--- R/twoPhaseDE.R
+++ R/twoPhaseDE.R
@@ -1,5 +1,3 @@
-### two phase DE test.
-### should allow user to provide a normalization factor matrix/vector
 twoPhaseDE <- function(sc2p.obj,
                        design, ## vector of covariate names
                        test.which, ## which of design to be tested?
@@ -7,30 +5,19 @@ twoPhaseDE <- function(sc2p.obj,
                        offset=c("SC2P", "sf")){
     Y <- assayData(sc2p.obj)$exprs
     Z <- assayData(sc2p.obj)$Z
+    Offset <- assayData(sc2p.obj)$Offset
 
-    ## determine offset
-    if(is.character(offset)) { ## offset is character
-        offset <- match.arg(offset)
-        if(offset == "sf") { # use size factor (derived from total counts)
-            k = colSums(Y)
-            k = log2(k/median(k))
-            Offset = matrix(rep(k, nrow(Y)), nrow=nrow(Y), byrow=TRUE)
-        } else ## use SC2P offset
-        Offset <- assayData(sc2p.obj)$Offset
-    } else if (is.vector(offset)) { ## offset is a vector
-        if(length(offset) != ncol(Y))
-            stop("offset has wrong length. Must equal to the number of cells.")
-        ## turn it into a matrix, replicate for each gene
-        Offset = matrix(rep(offset, nrow(Y)), nrow=nrow(Y), byrow=TRUE)
-    } else if (is.matrix(offset)) { ## offset is a matrix.
-        if( any(dim(offset) != dim(Y)) )
-            stop("offset has wrong dimension. Must have the same dimension as the input expression.")
-        Offset = offset
-    } else stop("Unrecognized option for offset.")
-
-
+    offset = match.arg(offset)
+    if(offset == "SC2P") {
+        Offset = assayData(sc2p.obj)$Offset
+    } else {
+        k = colSums(Y)
+        k = log2(k/median(k))
+        Offset = matrix(rep(k, nrow(Y)), nrow=nrow(Y), byrow=TRUE)
+    }
+    
     X <- pData(sc2p.obj)[, design, drop=F] ## or X can be permuted
-
+    
     twoPhaseDE0(Y=Y, Z=Z, X=X, Offset=Offset,
                 test.which=test.which, low.prob=low.prob)
 }
